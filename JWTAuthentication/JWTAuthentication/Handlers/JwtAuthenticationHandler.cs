23eaf111e73143eaa69f914584e7b1cf87a0a732
Vipin <Vipin@LAPTOP-NERL4B31>
2023-06-04 18:35:20 +0530

    sample dot net project of JWT authentication.

---
+++ JWTAuthentication/JWTAuthentication/Handlers/JwtAuthenticationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthentication.Handlers
+{
+    public class JwtAuthenticationHandler: IJwtAuthenticationHandler
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtAuthenticationHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+    }
+}
---
+++ JWTAuthentication/JWTAuthentication/Program.cs
@@ -0,0 +1,41 @@
+using JWTAuthentication.Handlers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using JWTAuthentication.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(configuration);
+
+builder.Services.AddTransient<IJwtAuthenticationHandler, JwtAuthenticationHandler>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
